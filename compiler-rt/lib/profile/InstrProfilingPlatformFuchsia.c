Manoj-Kumar-BV/llvm-project

commit c137d7c3840a8d31d04146f9298c6018bef11f2d

[profile] Don't publish VMO if there are no counters
If there are no counters, there's no need to publish the VMO.

Differential Revision: https://reviews.llvm.org/D102786

compiler-rt/lib/profile/InstrProfilingPlatformFuchsia.c
@@ -126,13 +126,20 @@ void __llvm_profile_initialize(void) {
 
   const __llvm_profile_data *DataBegin = __llvm_profile_begin_data();
   const __llvm_profile_data *DataEnd = __llvm_profile_end_data();
+  const uint64_t *CountersBegin = __llvm_profile_begin_counters();
+  const uint64_t *CountersEnd = __llvm_profile_end_counters();
   const uint64_t DataSize = __llvm_profile_get_data_size(DataBegin, DataEnd);
   const uint64_t CountersOffset =
       sizeof(__llvm_profile_header) + (DataSize * sizeof(__llvm_profile_data));
+  uint64_t CountersSize = CountersEnd - CountersBegin;
+
+  /* Don't publish a VMO if there are no counters. */
+  if (!CounterSize)
+    return;
 
   zx_status_t Status;
 
-  /* Create VMO to hold the profile data. */
+  /* Create a VMO to hold the profile data. */
   zx_handle_t Vmo = ZX_HANDLE_INVALID;
   Status = _zx_vmo_create(0, ZX_VMO_RESIZABLE, &Vmo);
   if (Status != ZX_OK) {

commit 05778907d1328575237e986774a863700125c0b1

[profile] Fix variable name
This fixes a bug introduced in c137d7c3840a8d31d04146f9298c6018bef11f2d.

compiler-rt/lib/profile/InstrProfilingPlatformFuchsia.c
@@ -134,7 +134,7 @@ void __llvm_profile_initialize(void) {
   uint64_t CountersSize = CountersEnd - CountersBegin;
 
   /* Don't publish a VMO if there are no counters. */
-  if (!CounterSize)
+  if (!CountersSize)
     return;
 
   zx_status_t Status;