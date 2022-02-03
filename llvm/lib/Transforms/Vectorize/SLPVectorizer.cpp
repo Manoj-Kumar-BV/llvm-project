Manoj-Kumar-BV/llvm-project

commit 510497f96f647f2dcde440888147feaa1ca14ed4

[SLP] Add basic self consistency asserts into scheduling
The idea here is to have a verify routine we can call during scheduling to ensure broken invariants are reported.  The intent is to help in debugging scheduling bugs.

At the moment, only the most basic properties are checked as adding several I thought held reported failures.

llvm/lib/Transforms/Vectorize/SLPVectorizer.cpp
@@ -2463,6 +2463,18 @@ private:
       Lane = -1;
     }
 
+    /// Verify basic self consistency properties
+    void verify() {
+      if (hasValidDependencies()) {
+        assert(UnscheduledDeps <= Dependencies && "invariant");
+      }
+
+      if (IsScheduled) {
+        assert(isSchedulingEntity() && UnscheduledDeps == 0 &&
+               "unexpected scheduled state");
+      }
+    }
+
     /// Returns true if the dependency information has been calculated.
     bool hasValidDependencies() const { return Dependencies != InvalidDeps; }
 
@@ -2710,6 +2722,15 @@ private:
           }
         }
       }
+    }
+
+    /// Verify basic self consistency properties of the data structure.
+    void verify() {
+      if (!ScheduleStart)
+        return;
+
+      for (auto *I = ScheduleStart; I != ScheduleEnd; I = I->getNextNode())
+        doForAllOpcodes(I, [](ScheduleData *SD) { SD->verify(); });
     }
 
     void doForAllOpcodes(Value *V,
@@ -7903,6 +7924,11 @@ void BoUpSLP::scheduleBlock(BlockScheduling *BS) {
     NumToSchedule--;
   }
   assert(NumToSchedule == 0 && "could not schedule all instructions");
+
+  // Check that we didn't break any of our invariants.
+#ifdef EXPENSIVE_CHECKS
+  BS->verify();
+#endif
 
   // Avoid duplicate scheduling of the block.
   BS->ScheduleStart = nullptr;

commit 1f70be32dcfd15c0de5a3d052af2a7465002a4bf

[SLP] Strengthen internal invariant assertions slightly
This builds on the invariant checks introduced in 510497f, and adds a couple more than seem to hold without additional work.

llvm/lib/Transforms/Vectorize/SLPVectorizer.cpp
@@ -2467,10 +2467,13 @@ private:
     void verify() {
       if (hasValidDependencies()) {
         assert(UnscheduledDeps <= Dependencies && "invariant");
+        assert(UnscheduledDeps <= FirstInBundle->UnscheduledDepsInBundle &&
+               "bundle must have at least as many dependencies as member");
       }
 
       if (IsScheduled) {
-        assert(isSchedulingEntity() && UnscheduledDeps == 0 &&
+        assert(isSchedulingEntity() && hasValidDependencies() &&
+               UnscheduledDeps == 0 &&
                "unexpected scheduled state");
       }
     }
@@ -2729,8 +2732,17 @@ private:
       if (!ScheduleStart)
         return;
 
-      for (auto *I = ScheduleStart; I != ScheduleEnd; I = I->getNextNode())
+      assert(ScheduleStart->getParent() == ScheduleEnd->getParent() &&
+             ScheduleStart->comesBefore(ScheduleEnd) &&
+             "Not a valid scheduling region?");
+
+      for (auto *I = ScheduleStart; I != ScheduleEnd; I = I->getNextNode()) {
+        auto *SD = getScheduleData(I);
+        assert(SD && "primary scheduledata must exist in window");
+        assert(isInSchedulingRegion(SD) &&
+               "primary schedule data not in window?");
         doForAllOpcodes(I, [](ScheduleData *SD) { SD->verify(); });
+      }
     }
 
     void doForAllOpcodes(Value *V,
@@ -3883,6 +3895,10 @@ void BoUpSLP::buildTree_rec(ArrayRef<Value *> VL, unsigned Depth,
   BlockScheduling &BS = *BSRef.get();
 
   Optional<ScheduleData *> Bundle = BS.tryScheduleBundle(VL, this, S);
+#ifdef EXPENSIVE_CHECKS
+  // Make sure we didn't break any internal invariants
+  BS.verify();
+#endif
   if (!Bundle) {
     LLVM_DEBUG(dbgs() << "SLP: We are not able to schedule this bundle!\n");
     assert((!BS.getScheduleData(VL0) ||