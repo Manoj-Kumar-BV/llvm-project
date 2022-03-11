Manoj-Kumar-BV/llvm-project

commit a7e329b78cab7d11009eea34bd088740b9feb952

Conditional include of missing headers under Z3

llvm/lib/Support/Z3Solver.cpp
@@ -13,7 +13,10 @@ using namespace llvm;
 
 #if LLVM_WITH_Z3
 
+#include "llvm/ADT/SmalLString.h"
 #include "llvm/ADT/Twine.h"
+
+#include <set>
 
 #include <z3.h>
 

commit 4c0bf5439dc53e170d7630ec8e334f90b6d16a00

[analyzer] Fix include typo introduced by a7e329b

llvm/lib/Support/Z3Solver.cpp
@@ -13,7 +13,7 @@ using namespace llvm;
 
 #if LLVM_WITH_Z3
 
-#include "llvm/ADT/SmalLString.h"
+#include "llvm/ADT/SmallString.h"
 #include "llvm/ADT/Twine.h"
 
 #include <set>