Manoj-Kumar-BV/llvm-project

commit d182fbb9437c15fe02738fc37db1b65d9883e9fb

Fix an unused-variable warning in release build, NFC

llvm/lib/Transforms/IPO/AttributorAttributes.cpp
@@ -3722,8 +3722,8 @@ struct AAIsDeadFloating : public AAIsDeadValueImpl {
       // still needed.
       if (auto *SI = dyn_cast<StoreInst>(I)) {
         SmallSetVector<Instruction *, 8> AssumeOnlyInst;
-        bool IsDead = isDeadStore(A, *SI, &AssumeOnlyInst);
-        assert(IsDead && "Store was assumed to be dead!");
+        assert(isDeadStore(A, *SI, &AssumeOnlyInst) &&
+               "Store was assumed to be dead!");
         A.deleteAfterManifest(*I);
         for (size_t i = 0; i < AssumeOnlyInst.size(); ++i) {
           Instruction *AOI = AssumeOnlyInst[i];

commit ac64174f8ac3c2e368c3473dc40cb3537e2d13cd

[Attributor] Fix a break in relase build caused by d182fbb9437c15fe02738fc37db1b65d9883e9fb.

llvm/lib/Transforms/IPO/AttributorAttributes.cpp
@@ -3722,8 +3722,9 @@ struct AAIsDeadFloating : public AAIsDeadValueImpl {
       // still needed.
       if (auto *SI = dyn_cast<StoreInst>(I)) {
         SmallSetVector<Instruction *, 8> AssumeOnlyInst;
-        assert(isDeadStore(A, *SI, &AssumeOnlyInst) &&
-               "Store was assumed to be dead!");
+        bool IsDead = isDeadStore(A, *SI, &AssumeOnlyInst);
+        (void)IsDead;
+        assert(IsDead && "Store was assumed to be dead!");
         A.deleteAfterManifest(*I);
         for (size_t i = 0; i < AssumeOnlyInst.size(); ++i) {
           Instruction *AOI = AssumeOnlyInst[i];