Manoj-Kumar-BV/llvm-project

commit 1ff58d2c583aa22b9922cfb58000d161811a8018

[C++20] [Modules] Profile TemplateName by canonical decl
Close https://github.com/llvm/llvm-project/issues/61317

The root cause of the problem is that we profile TemplateName by the
non-canonical decls so that the compiler thought they are two different
types. But this is not true. We fixed the issue after we profile the
template name by using the same name.

clang/lib/AST/TemplateName.cpp
@@ -281,6 +281,13 @@ bool TemplateName::containsUnexpandedParameterPack() const {
   return getDependence() & TemplateNameDependence::UnexpandedPack;
 }
 
+void TemplateName::Profile(llvm::FoldingSetNodeID &ID) {
+  if (auto *TD = getAsTemplateDecl())
+    ID.AddPointer(TD->getCanonicalDecl());
+  else
+    ID.AddPointer(Storage.getOpaqueValue());
+}
+
 void TemplateName::print(raw_ostream &OS, const PrintingPolicy &Policy,
                          Qualified Qual) const {
   auto Kind = getKind();

clang/include/clang/AST/TemplateName.h
@@ -351,9 +351,7 @@ public:
   /// error.
   void dump() const;
 
-  void Profile(llvm::FoldingSetNodeID &ID) {
-    ID.AddPointer(Storage.getOpaqueValue());
-  }
+  void Profile(llvm::FoldingSetNodeID &ID);
 
   /// Retrieve the template name as a void pointer.
   void *getAsVoidPointer() const { return Storage.getOpaqueValue(); }

clang/test/Modules/pr61317.cppm
@@ -1,0 +1,49 @@
+// From https://github.com/llvm/llvm-project/issues/61317
+// RUN: rm -rf %t
+// RUN: mkdir -p %t
+// RUN: split-file %s %t
+//
+// RUN: %clang_cc1 -std=c++20 %t/A.cppm -emit-module-interface -o %t/A.pcm
+// RUN: %clang_cc1 -std=c++20 %t/B.cppm -emit-module-interface -o %t/B.pcm \
+// RUN:     -fprebuilt-module-path=%t
+// RUN: %clang_cc1 -std=c++20 %t/Use.cpp -fprebuilt-module-path=%t -fsyntax-only -verify
+
+//--- foo.h
+#ifndef _FOO
+#define _FOO
+
+template <typename T> struct Foo {
+  Foo(T) {}
+};
+
+template <typename T> Foo(T&) -> Foo<T>;
+
+struct Bar {
+  template <typename T>
+    requires requires { Foo{T()}; }
+  void baz() const {}
+};
+
+#endif
+
+//--- A.cppm
+module;
+#include "foo.h"
+export module A;
+export using ::Foo;
+export using ::Bar;
+
+//--- B.cppm
+module;
+#include "foo.h"
+export module B;
+export import A;
+
+//--- Use.cpp
+// expected-no-diagnostics
+import A;
+import B;
+void use() {
+  Bar _; 
+  _.baz<int>();
+}

commit 1cca089003e140837123c1379ea1a42eb02a7c3a

[clang] Fix a UsingTemplate regression after 1ff58d2c583aa22b9922cfb58000d161811a8018
TemplateName::getAsTemplateDecl() returns the underlying TemplateDecl
for a UsingTemplate kind template name. We should respect that in the
Profile method otherwise we might desugar the template name unexpectedly
(e.g. for template argument deduction with deduciton guides).

Differential Revision: https://reviews.llvm.org/D146202

clang/lib/AST/TemplateName.cpp
@@ -282,7 +282,9 @@ bool TemplateName::containsUnexpandedParameterPack() const {
 }
 
 void TemplateName::Profile(llvm::FoldingSetNodeID &ID) {
-  if (auto *TD = getAsTemplateDecl())
+  if (const auto* USD = getAsUsingShadowDecl())
+    ID.AddPointer(USD->getCanonicalDecl());
+  else if (const auto *TD = getAsTemplateDecl())
     ID.AddPointer(TD->getCanonicalDecl());
   else
     ID.AddPointer(Storage.getOpaqueValue());

clang-tools-extra/include-cleaner/unittests/WalkASTTest.cpp
@@ -171,6 +171,13 @@ TEST(WalkAST, TemplateNames) {
       namespace ns {template <typename> struct S {}; }
       using ns::$explicit^S;)cpp",
            "^S<int> x;");
+  testWalk(R"cpp(
+      namespace ns {
+        template <typename T> struct S { S(T);};
+        template <typename T> S(T t) -> S<T>;
+      }
+      using ns::$explicit^S;)cpp",
+      "^S x(123);");
   testWalk("template<typename> struct $explicit^S {};",
            R"cpp(
       template <template <typename> typename> struct X {};

clang/test/AST/ast-dump-using-template.cpp
@@ -9,8 +9,11 @@ template<typename T> class S {
  public:
    S(T);
 };
+template<typename T> struct S2 { S2(T); };
+template <typename T> S2(T t) -> S2<T>;
 }
 using ns::S;
+using ns::S2;
 
 // TemplateName in TemplateSpecializationType.
 template<typename T>
@@ -36,3 +39,10 @@ using C = decltype(DeducedTemplateSpecializationT);
 // CHECK-NEXT:  |-DeclRefExpr {{.*}}
 // CHECK-NEXT:  `-ElaboratedType {{.*}} 'S<int>' sugar
 // CHECK-NEXT:    `-DeducedTemplateSpecializationType {{.*}} 'ns::S<int>' sugar using
+
+S2 DeducedTemplateSpecializationT2(123);
+using D = decltype(DeducedTemplateSpecializationT2);
+// CHECK:      DecltypeType {{.*}}
+// CHECK-NEXT:  |-DeclRefExpr {{.*}}
+// CHECK-NEXT:  `-ElaboratedType {{.*}} 'S2<int>' sugar
+// CHECK-NEXT:    `-DeducedTemplateSpecializationType {{.*}} 'S2<int>' sugar using